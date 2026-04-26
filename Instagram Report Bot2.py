9753724cf53bf214aab38a0e3fae311a3a0bd403
mumimu99 <memobanna89@icloud.com>
2026-04-26 04:31:50 +0300

    Update Instagram Report Bot2.py

--- Instagram Report Bot2.py
+++ Instagram Report Bot2.py
@@ -10,7 +10,6 @@ def getOptions(args=sys.argv[1:]):
 
     parser = argparse.ArgumentParser(description="This bot helps users to mass report accounts with clickbaits or objectionable material.")
     parser.add_argument("-u", "--username", type = str, default = "", help = "Username to report.")
-    parser.add_argument("-f", "--file", type = str, default = "acc.txt", help = "Accounts list ( Defaults to acc.txt in program directory ).")
 
     options = parser.parse_args(args)
 
@@ -19,11 +18,10 @@ def getOptions(args=sys.argv[1:]):
 
 args = getOptions()
 
-username = args.username
-acc_file = args.file
+username = args.ibr.mhj
 
 if username == "" :
-	username = input("Username: ")
+	username = input("ibr.mhj: ")
 
 a = open(acc_file, "r").readlines()
 file = [s.rstrip()for s in a]
